1d93c983060c5995aa34e977caa87b97ba5cc08b
Dolores Miao <wjmiao@ucdavis.edu>
2024-02-01 09:59:49 -0800

    argparse integration

--- driver/auto_gdb.py
+++ driver/auto_gdb.py
@@ -4,50 +4,75 @@ import time
 import json
 import re
 import pexpect
+import argparse
 
 gdb = None
 count = 0
+Verbose = 2
 
-def recv():
+def prt(*args, **kwargs):
+    # level 0 - 3: error, warning, info, low-priority info
+    # default prt level: info (2)
+    global Verbose
+    level = 3
+    if "level" in kwargs:
+        level = kwargs["level"]
+        kwargs.pop("level")
+    if Verbose >= level:
+        print(*args, **kwargs)
+
+def recv(display):
     global gdb
     gdb.expect(r'\(gdb\)')
     text = gdb.before.decode('utf-8')
     ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
     text = ansi_escape.sub('', text)
-    print("text:", text)
+    if display:
+        prt("text:", text, level=2)
     return text
 
-def send(*txt):
+def send(*txt, **kwargs):
     global gdb
     global count
     count += 1
-    while True:
-        #try:
-        gdb.sendline(' '.join(txt))
-        time.sleep(0.001)
-        break
-        #except BlockingIOError as e:
-        #    print("IO blocked. Retry...")
-    allText = recv()
+    display = True
+    if "display" in kwargs:
+        display = kwargs["display"]
+
+    sendText = ' '.join(txt)
+    prt("send:", sendText, level=3)
+    gdb.sendline(sendText)
+    #time.sleep(0.001)
+    allText = recv(display)
     return allText
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--program", type=str, help="the program to be tested", required=True)
+    parser.add_argument("-a", "--args", nargs='+', help="Program arguments", required=True)
+    parser.add_argument("-v", "--verbose", type=int, choices=[0, 1, 2, 3], default=2, help="set output verbosity (0=error, 1=warning, 2=info, 3=low priority info)")
+    args = parser.parse_args()
+    Verbose = args.verbose
+    ProgramName = args.program
+    Arguments = args.args
+    print("program:", ProgramName, "args:", Arguments)
+
     gdb = pexpect.spawn('gdb')
     gdb.delaybeforesend = None
     gdb.delayafterread = None
     gdb.delayafterclose = None
     gdb.delayafterterminate = None    
-    
+    recv(True)
     send("set", "pagination", "off")
+    #send("set", "disassemble-next-line", "on")
     
-    send("set", "disassemble-next-line", "on")
-    
-    send("file", sys.argv[1])
+    send("file", ProgramName)
     
     send("b", "main")
     
-    send("r ", sys.argv[2])
-            
+    send("r ", *Arguments)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    prev_inst = ""
     while True:    
         allText = send("si")
         endOfProgram = False
@@ -57,8 +82,10 @@ if __name__ == "__main__":
         if endOfProgram:
             break
 
-        allText = send("x/i $pc")
+        curr_inst = send("x/i $pc", display=False)
         allText = send("bt -frame-info location-and-address")
         allText = send("info locals")
+        prt("curr_inst:", curr_inst, level=2)
+        prev_inst = curr_inst
 
         input("continue...")
